91be6755d4f41f0f82ac3ec85900a565c23bdbba
lcasaus <80173456+eriklindes@users.noreply.github.com>
2023-09-21 08:14:47 +0200

    Initial structure

---
+++ script.js
@@ -0,0 +1,58 @@
+
+const restartButton = document.querySelector("#restart-button");
+const boardHTML = document.querySelector("gameboard").innerHTML
+
+// Player object - using factory
+
+const Player = (name) => {
+    const getName = () => name;
+    const win = () {
+        // update the html and stop the game
+    }
+
+    const play = () {
+        // update html with the turn
+
+    }
+}
+
+
+// Gameboard object (including gameboard array)
+
+const Gameboard = () => {
+    let gameboard = [];
+    let
+
+    const render )= () =>
+}
+
+
+
+const Game = (() => {
+    let currentPlayerIndex = 0;
+    let gameOver = false;
+})();
+
+
+
+
+// Restart game
+
+restartButton.addEventListener("click", () => {
+    restart();
+}
+
+function restart () {
+
+}
+
+
+// Define players & kick-off the game
+
+const player_x = Player("X");
+const player_o = Player("O");
+player_x.play();
+
+
+
+
